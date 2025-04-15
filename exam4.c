a0e0af8ddb4000b633fb780ea8cde896d4382cec
test@test.com <test>
2025-04-15 12:55:43 +0900

    before launch fin

--- exam4.c
+++ exam4.c
@@ -380,7 +380,9 @@ int main(void)
 
 #if 0 
 #include <stdio.h>
+
 void print_ary(int* pa);
+
 int main(void)
 {
 	int ary[5] = { 10, 20, 30, 40, 50 };
@@ -532,56 +534,64 @@ void printNum(int* pa, int size) {
 // [10-1] 정렬 알고리즘
 /***********************************************************/
 
-#if 1 
+#if 0 
 #include <stdio.h>
 
 void swap(int* pa, int* pb);
 void bubbleSort(int* pa, int size);
 void print_array(int* pa, int size);
 void selection_sort(int* pa, int size);
-void binary_search(int *pa, int size, int search);
+void binary_search(int* pa, int size, int search);
 int main(void)
 {
 	int arr[10] = { 1, 32, 19, 27, 23, 20, 15, 96, 25, 37 };
 	int size = sizeof(arr) / sizeof(int);
 	printf("배열 원본 : ");
-	print_array(arr, size);	
-	//bubbleSort(arr, size);
-	//print_array(arr, size);
-	//selection_sort(arr, size);
-	//print_array(arr, size);
+	print_array(arr, size);
+	// bubbleSort(arr, size);
+	// print_array(arr, size);
+	selection_sort(arr, size);
+	print_array(arr, size);
 
 	int search = 19;
-	binary_search(arr, size, search);	
+	binary_search(arr, size, search);
 	print_array(arr, size);
 	return 0;
 }
 
-void print_array(int* pa, int size) {
-	for (int i = 0;i < size;i++) {
+void print_array(int* pa, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
 		printf("%d ", pa[i]);
 	}
 	printf("\n");
 }
-//두 값을 치환하는 swap
-void swap(int* pa, int* pb) {
+// 두 값을 치환하는 swap
+void swap(int* pa, int* pb)
+{
 	int temp;
 	temp = *pa;
 	*pa = *pb;
 	*pb = temp;
 }
-void bubbleSort(int* pa, int size) {
+void bubbleSort(int* pa, int size)
+{
 	int max = pa[0];
-	for (int i = 1;i < size;i++) {
-		for (int j = 0;j < i;j++) {
-			if (pa[i] < pa[j]) swap(&pa[i], &pa[j]);
+	for (int i = 1; i < size; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (pa[i] < pa[j])
+				swap(&pa[i], &pa[j]);
 		}
 	}
 }
-void selection_sort(int* pa, int size) {
-	//선택정렬
-	//버블정렬의 단점을 보완
-	//사이클 당 최대 치환 1번
+void selection_sort(int* pa, int size)
+{
+	// 선택정렬
+	// 버블정렬의 단점을 보완
+	// 사이클 당 최대 치환 1번
 	int max;
 	int max_index;
 	for (int j = 0; j < size - 1; j++)
@@ -600,38 +610,82 @@ void selection_sort(int* pa, int size) {
 		swap(&pa[max_index], &pa[size - (j + 1)]);
 	}
 }
-void binary_search(int* pa, int size, int search)	//왜 안됨???
+void binary_search(int* pa, int size, int search) // 왜 안됨???
 {
 	// 이진탐색
-	int low_index = 0; // 0
-	int high_index = size - 1; // 9
+	int low_index = 0;                            // 0
+	int high_index = size - 1;                    // 9
 	int mid_index = (low_index + high_index) / 2; // 4
+	
+	//while문이 10000번 정도 반복된다. ->
+	int count = 0;
 
-	while (1)
+	while (low_index <= high_index)	//1대신 넣음. low_index가 high_index보다 커지는 경우(search가 없는 경우) 방지. 
 	{
+		int mid_index = (low_index + high_index) / 2;
 		// 23       >      19
 		if (pa[mid_index] > search)
 		{
 			// low_index는 그대로
 			high_index = mid_index - 1;
-			mid_index = (low_index + high_index) / 2;
 		}
 		else if (pa[mid_index] < search)
 		{
 			// high_index는 그대로
 			low_index = mid_index + 1;
-			mid_index = (low_index + high_index) / 2;
 		}
 		else
 		{
 			printf("찾고자 하는 %d은 index %d에 있습니다", search, mid_index);
-			break;
+			return;
 		}
 	}
 }
 #endif
 /***********************************************************/
-// [0-0] 템플릿
+// [11-1] 대문자를 소문자로 변경 
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+
+char lowercase(char alphabet);
+
+int main(void)
+{
+	printf("%c\n", lowercase('H'));
+	return 0;
+}
+
+char lowercase(char alphabet) {
+
+	if (alphabet <= 'Z' && alphabet >= 'A') return alphabet - 'A' + 'a';
+	else return alphabet;
+}
+#endif
+
+/***********************************************************/
+// [11-2] 공백이나 제어문자의 입력 
+/***********************************************************/
+
+#if 0 
+#include <stdio.h>
+
+int main(void)
+{
+	char ch, ch2;
+	//scanf("%c", &ch);
+	//printf("[%c]\n", ch);	//엔터 등도 문자로 취급
+	//printf("[%d]\n", ch);
+
+	scanf("%c%c", &ch, &ch2);
+	printf("[%d] [%d]\n", ch, ch2);
+	return 0;
+}
+#endif
+
+/***********************************************************/
+// [11-3] Hello world 출력 
 /***********************************************************/
 
 #if 0
@@ -639,14 +693,66 @@ void binary_search(int* pa, int size, int search)	//왜 안됨???
 
 int main(void)
 {
+	while (1) {
+		char ch;
+		//scanf -> 문자입력, 문자열, 정수, 실수
+		//문자 : 1byte, 정수/실수 => 4, 8byte
+		//버퍼를 사용하기 때문에 상대적으로 무거움.
+		scanf("%c", &ch);
+		if (ch == 10) break; //엔터 입력 전까지 입력받기, 아스키 코드 10 == enter
+		printf("%c", ch);
+	}
+	return 0;
+}
+#endif
 
+/***********************************************************/
+// [11-3] getchar, putchar 
+/***********************************************************/
+//문자 한정으로 scanf보다 가볍게 사용할 수 있다.
+#if 0
+#include <stdio.h>
+
+int main(void)
+{
+	int ch;	
+	while (1) {
+		ch = getchar();
+		if (ch == 10) break;
+		printf("입력한 문자 : ");
+		putchar(ch);
+	}
+		return 0;
+}
+#endif
+
+/***********************************************************/
+// [11-3] scanf에 공백
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0;i < 5;i++) {
+		char ch;
+		//scanf("%c", &ch);
+		scanf(" %c", &ch);	//화이트 스페이스를 제외하고 입력받음. ex. h e l l o로 입력받아도 hello로 출력됨.
+		//%앞에 공백 삽입 시 -> 화잍트 스페이스 무시!!
+		// 입력 값 : H e l l o
+		// 입력 값 : H엔터e엔터l엔터l엔터o엔터
+		// 입력 값 : H탭e탭l탭l탭o탭
+		//3개 모두 출력은 Hello
+		printf("%c", ch);
+	}
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [11-4] 버퍼를 사용하는 문자 입력 
 /***********************************************************/
 
 #if 0
@@ -654,14 +760,93 @@ int main(void)
 
 int main(void)
 {
-
+	// 3개의 문자를 입력받는
+	//for (int i = 0;i < 3;i++) {
+	//	char ch;
+	//	scanf("%c", &ch);
+	//	printf("%c", ch);
+	//}
+	
+	while(1) {
+		char ch;
+		//엔터가 들어오기 전까지 입력!!
+		//엔터 들어올 시 break;
+		scanf("%c", &ch);	
+		if (ch == '\n') break;
+		printf("%c", ch);
+	}
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [11-5] 입력 문자의 아스키 코드 값을 출력하는 프로그램.
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+
+int main(void)
+{	
+	int result;
+	char ch;
+
+	while (1)
+	{
+		result = scanf("%c", &ch);
+		//if (result == -1) break;	//crtl + z가 -1의 값을 가짐
+		if (result == EOF) break;
+		printf("%d ", ch);
+	}
+	return 0;
+}
+#endif
+
+/***********************************************************/
+// [11-6] getchar 함수를 사용한 문자열 입력
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+
+//apple -> 문자열을 입력받는 함수!!
+void my_getchar(char* str, int size);
+
+int main(void)
+{
+	char str[7];
+	my_getchar(str, 7);
+	printf("입력한 문자열 : %s \n", str);
+	
+	return 0;
+}
+
+void my_getchar(char* str, int size)
+{
+	//int i = 0;	//이 코드는 사용자가 6글자보다 적게 입력해도 6글자를 모두 입력받아야하는 문제 존재
+	//int ch;
+	//while (i < size-1) {
+	//	//scanf("%c", &str[i]);
+	//	str[i] = getchar();
+	//	i++;
+	//}
+	//str[i] = '\0';
+	
+	int i;
+	for (i = 0;i < size - 1;i++) {
+		str[i] = getchar();
+		if (str[i] == '\n') {
+			break;
+			i++;
+		}
+	}
+	str[i] = '\0';	//끝에 널문자 삽입.
+}
+#endif
+
+/***********************************************************/
+// [11-7] 버퍼 초기화
 /***********************************************************/
 
 #if 0
@@ -669,14 +854,83 @@ int main(void)
 
 int main(void)
 {
+	int num;
+	int grade;
+	//scanf()과 getchar()는 buffer를 공유한다.
+	printf("학번 입력 : ");
+	scanf("%d", &num);
 
+	//getchar();	//버퍼 초기화
+
+	printf("학점 입력 : ");
+	grade = getchar();	//학점 입력이 안됨. 앞에 학번에서 입력하고 남아있는 \n을 버퍼에서 가져와서 끝남.
+
+	printf("학번 : %d, 학점 : %c\n", num, grade);	//학점을 %d로 하면 엔터에 해당하는 \n가 출력되는 것을 볼 수 있다.
+	
+	//입력 버퍼 사용시 주의사항
+	//잘 모르겠으면 버퍼를 초기화 하자.
+	//getchar()를 한번 호출하면 초기화된다.
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [11-도전] 길이가 가장 긴 단어 찾기
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+//scanf의 %s의 사용 금지. string.h 쓰지 않기
+int main(void)
+{
+	int cnt = 0;
+	int ch;
+	int result = 0;
+	while (1) {
+		ch = getchar();
+		if (ch == EOF) break;
+		else if (ch != '\n') {
+			cnt ++;
+		}
+		else if (ch == '\n') {
+			//getchar(); 줄바꿈문자가 버퍼에 있다고 생각해서 추가한 건데, 이미 위에서 줄바꿈문자를 읽은 상태이므로 필요없음.
+			if (result < cnt) {
+				result = cnt;
+			}
+			cnt = 0;
+		}
+			
+	}
+
+	printf("가장 긴 단어의 길이 : %d", result);
+	return 0;
+	/*교수님 코드
+	   int ch, max, len;
+		max = -1;
+		while (1)
+		{
+		ch = getchar();
+		if (ch == EOF) break;
+
+      // 하나의 문자열을 입력!!
+      len = 0;
+      while (1)
+      {
+         if (ch == '\n') break;
+         ch = getchar();
+         len++;
+      }
+      // printf("해당 문자열의 길이 : %d\n", len);
+      if (len > max) max = len;
+   }
+   printf("가장 긴 단어의 길이 : %d\n", max);
+	*/
+}
+#endif
+
+/***********************************************************/
+// [12-1] 문자열은 문자열 배열 ??
 /***********************************************************/
 
 #if 0
@@ -684,29 +938,61 @@ int main(void)
 
 int main(void)
 {
+	int array[5] = { 10, 20, 30, 40, 50 };
+	int* pa = array;
+	printf("%p\n", array);	//배열명은 주솟값이다.
+	printf("%p\n", pa);
 
+	char fruit[6] = { 'a', 'p', 'p', 'l', 'e' };	//문자열 가장 뒤에 널문자를 컴파일러가 삽입함.
+	char fruit2[6] = "apple";	//문자열도 포인터이다.
+
+	printf("%s\n", fruit);
+	printf("%s\n", fruit2);
+
+	char* pf = fruit;
+	char* pf2 = fruit2;
+
+	printf("%p\n", pf);		//다른 주소를 가지고 있다. = 다른 메모리 위치를 가진다.
+	printf("%p\n", pf2);
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [12-2] 포인터로 문자열을 사용하는 방법
 /***********************************************************/
-
+// 사용하는 이유 : 배열을 함수에서 접근하는 유일한 방법이 포인터인데 문자열은 배열임.
 #if 0
 #include <stdio.h>
 
 int main(void)
 {
+	char drink[80] = "tejava";
+	char* pd = drink;
 
+	//포인터를 통해 문자열 접근 가능
+	//printf("%c\n", pd[0]);
+	
+	//while (1) {
+	//	if (*pd == '\0') break;
+	//	printf("%c", *pd);
+	//	pd++;
+	//}
+
+	while (*pd != '\0') {	//*pd 만 있어도 가능.(널이면 0이므로)
+		printf("%c", *(pd++));
+		//pd++
+	}
+
+	//while (*pd) printf("%c", *(pd++));
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [12-잠깐] 같은 문자열 상수
 /***********************************************************/
 
 #if 0
@@ -714,29 +1000,51 @@ int main(void)
 
 int main(void)
 {
+	char fruit[6] = { 'a', 'p', 'p', 'l', 'e', '\0' };
+	char fruit2[6] = "apple";
+	char fruit3[6] = "apple";
 
+	printf("apple의 주소 : %p\n", "apple");	//이거 두개는 같다. 두 개의 문자열을 사용했지만 컴파일러가 최적화를 위해 하나의 메모리를 사용
+	printf("apple의 주소 : %p\n", "apple");
+
+	printf("%p\n", fruit);
+	printf("%p\n", fruit2);
+	printf("%p\n", fruit3);
+	printf("\n");
+
+	char* pf = fruit;
+	char* pf2 = fruit2;
+	char* pf3 = fruit3;
+
+	printf("%p\n", pf);
+	printf("%p\n", pf2);
+	printf("%p\n", pf3);
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [12-3] scanf 함수를 사용한 문자열 입력
 /***********************************************************/
 
-#if 0
+#if 0 
 #include <stdio.h>
 
 int main(void)
 {
-
+	char str[80];
+	
+	printf("문자열 입력 : ");
+	scanf("%s", str);	//공백없는 문자열만 입력받음.
+	printf("%s\n", str);
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [12-4] gets 함수로 한 줄의 문자열 입력 
 /***********************************************************/
 
 #if 0
@@ -744,14 +1052,41 @@ int main(void)
 
 int main(void)
 {
-
+	char str[80];
+	printf("문자열 입력 : ");
+	gets(str);	//엔터를 제외한 탭, 공백 입력 가능
+	//문자열의 갯수를 넘어서 입력받을 위험성이 있다.
+	printf("%s\n", str);
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [12-5] fgets함수의 문자열 입력 방법
+/***********************************************************/
+
+#if 0
+#include <stdio.h>
+#include <string.h>	//strlen()
+
+int main(void)
+{
+	char str[10];
+	printf("문자열 입력 : ");
+	fgets(str, sizeof(str), stdin);	//키보드로 입력하겠다라는 뜻, 널문자를 제외한 10 -1만큼 입력됨.
+	printf("문자열 길이 : %d\n", strlen(str)-1);	//엔터 제외
+	printf("문자열 길이2 : %d\n", sizeof(str));	//문자열 선언 당시 선언된 크기(실제 길이와는 무관)
+	printf("입력한 문자열 : %s\n", str);
+	//문자열을 제외한 자료구조 배열에서는
+	//배열의 요소 갯수를 sizeof(배열명) / sizeof(자료형)
+	//문자열은 널문자를 제외해야 한다.
+	return 0;
+}
+#endif
+
+/***********************************************************/
+// [12-6] 개행 문자로 인해 get함수가 입력안되는 경우
 /***********************************************************/
 
 #if 0
@@ -759,14 +1094,27 @@ int main(void)
 
 int main(void)
 {
+	int age;
+	char name[20];
+	printf("나이 입력 : ");
+	scanf("%d", &age);
 
+	printf("이름 입력 : ");
+	//get함수 사용 전 버퍼 초기화 필요
+	//초기화 방법에는 3가지가 있다. 교재 358p
+	//scanf("%*c");
+	//getchar();
+	fgetc(stdin);
+	gets(name);
+
+	printf("나이 : %d, 이름 : %s", age, name);
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [12-7] 문자열을 출력하는 puts와 fputs 함수
 /***********************************************************/
 
 #if 0
@@ -774,14 +1122,21 @@ int main(void)
 
 int main(void)
 {
+	char str[80] = "apple juice";	//배열에 문자열 초기화
+	char *ps = "banana";	//*ps : 배열이라는 뜻.
 
+	puts(str);	//자동 줄바꿈.
+	puts(ps);
+
+	fputs(str, stdout);	//stdout : 모니터를 통해 표준 출력
+	fputs(ps, stdout);	//fgets는 줄 바꿈이 안됨.
 
 	return 0;
 }
 #endif
 
 /***********************************************************/
-// [0-0] 템플릿
+// [12-직접] gets함수 구현해보기
 /***********************************************************/
 
 #if 0
@@ -789,158 +1144,31 @@ int main(void)
 
 int main(void)
 {
+	char str[20];	//"apple" 값을 입력!!
+	//getchar() 함수를 활용해 gets() 구현
+	//getchar() -> 문자를 하나하나 입력!
 
+	//gets() 시작
+	//int i = 0;
+	//while (i<20) {
+	//	str[i] = getchar();
+	//	if (str[i++] == '\n') break;
+	//}
+	//str[i] = '\0';
+	////gets() 끝
 
-	return 0;
-}
-#endif
+	//printf("%s", str); //apple 출력
 
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
+	//return 0;
 
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	r/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-eturn 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	return 0;
-}
-#endif
-
-/***********************************************************/
-// [0-0] 템플릿
-/***********************************************************/
-
-#if 0
-#include <stdio.h>
-
-int main(void)
-{
-
-
+	//포인터로 gets 구현
+	char* ps = str;
+	while (1) {
+		*ps = getchar();
+		if (*ps++ == '\n') break;
+	}
+	*ps = '\0';
+	printf("%s", str);
 	return 0;
 }
 #endif
